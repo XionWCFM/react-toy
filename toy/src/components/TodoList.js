3b11c2792b3fb0c767584f2e04fa106a513e8422
XionWCFM <101111364+XionWCFM@users.noreply.github.com>
2023-02-28 23:57:23 +0900

    mockup and styling

---
+++ toy/src/components/TodoList.js
@@ -0,0 +1,15 @@
+import './TodoList.scss';
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+
+export default TodoList;
